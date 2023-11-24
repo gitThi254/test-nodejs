99ce9c959f1cf0728192f5a42a9c81770adf7bad
Thi <anhthix7c@gmail.com>
2023-11-25 00:04:13 +0700

    verion-2

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 require("dotenv").config();
-
+const home = require("./routes/home");
 const port = process.env.PORT || 9001;
 
 const app = express();
+app.use(express.json());
+app.use("/home", home);
 
 app.listen(port, () => {
   console.log(`server running at ${port}`);
